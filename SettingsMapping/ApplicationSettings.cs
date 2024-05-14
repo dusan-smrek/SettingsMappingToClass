09a170165a5df91760915cf90c2dd594179397ed
Dušan Smrek <63956080+dusan-smrek@users.noreply.github.com>
2024-05-14 06:37:20 +0200

    Initial commit.

---
+++ SettingsMapping/ApplicationSettings.cs
@@ -0,0 +1,7 @@
+namespace WebApi.Helpers
+{
+    public class ApplicationSettings
+    {
+        public string SecretKey { get; set; } = string.Empty;
+    }
+}
---
+++ SettingsMapping/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using WebApi.Helpers;
+
+Console.WriteLine("Hello, Settings Mapping World!");
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false).Build();
+
+var appSettings = config.GetSection("ApplicationSettings").Get<ApplicationSettings>();
+
+Console.WriteLine($"App settings: secret = '{appSettings?.SecretKey}'");
